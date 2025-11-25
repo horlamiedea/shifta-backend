ca1f2aef2c897cc394ba46a55462c6e1c4fa001c
horlamiedea <horlamiedea@gmail.com>
2025-11-25 20:39:14 +0100

    allowed origin

--- shifta_project/settings.py
+++ shifta_project/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = "django-insecure-t7i^fltw=fd6=sq4%$c+)2d8_=bst-m4-n15#^#4w9^v9-^6%5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://urahealth.westus2.cloudapp.azure.com/"]
+ALLOWED_HOSTS = ["urahealth.westus2.cloudapp.azure.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
